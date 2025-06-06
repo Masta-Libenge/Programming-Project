e981276d0721e187197ac6b8b93c9be68c292ef0
Masta-Libenge <masta.libenge@student.ehb.be>
2025-06-06 10:29:07 +0200

    Controller aangemaakt voor elke type user

--- my-laravel-app/app/Http/Controllers/Auth/LoginController.php
+++ my-laravel-app/app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,25 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    { //Hier wordt in de DB gecheckt of deze persoons account bestaat.
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'role' => 'required|in:student,admin,bedrijf',
+        ]);
+
+        $user = User::where('email', $request->email)
+            ->where('type', $request->role)
+            ->first();
+        // Redirect based on role
+        return match ($user->type) {
+            'student' => redirect()->route('student.dashboard'),
+            'admin' => redirect()->route('admin.dashboard'),
+            'bedrijf' => redirect()->route('bedrijf.dashboard'),
+        };
+    }
+
+
+
 }
